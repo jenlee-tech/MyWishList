5a95250823e14ca0223d86576d6f15ef5fd90e81
Jennifer Lee <jenlee.tech@gmail.com>
2021-11-11 10:29:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return ( 
+    <div>
+        <h1> My Wishlist </h1> 
+        <button>Close</button>
+        <form>
+          <div>
+            <label>Name of Gift:
+              <input type="text" name="name"/>
+            </label> 
+          </div>
+
+          <div>
+            <label>Description:
+              <input type="text" name="description"/>
+            </label>
+          </div>
+
+
+          <div>
+            <label>Link:
+              <input type="url" name="link"/>
+            </label>
+          </div>
+          
+          <input type="submit" value="Add" />
+        </form>
+
+
     </div>
-  );
+    );
 }
 
 export default App;
