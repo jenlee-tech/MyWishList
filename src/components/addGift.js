85cc8d9cd2cef572d9db7decd0a4a0fed21ebfdb
Jennifer Lee <jenlee.tech@gmail.com>
2021-11-22 10:59:47 -0500

    added useState and functions to the form

--- src/components/addGift.js
+++ src/components/addGift.js
@@ -2,34 +2,49 @@ import React, { useState } from 'react';
 
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("The submit button got clicked!  What happen?")
+    //console.log("The submit button got clicked!  What happen?")
+
+
   }
 
 
 const AddGift = () => {
+    const [name, setName] = useState('')
+    const [descrip, setDescrip] = useState('')
+    const [link, setLink] = useState('')
+
 
 return (
         <form>
         <div>
         <label>Name of Gift:   </label> 
-            <input type="text" name="name" placeholder="Name of Gift" />
+            <input 
+                type="text" 
+                name={name} 
+                onChange={(e) => setName(e.target.value)} 
+                placeholder="Name of Gift" />
 
         </div>
 
         <div>
         <label>Description:</label>
-            <input type="text" name="description" placeholder="description" />
-        
+            <input 
+                type="text" 
+                name={descrip} 
+                onChange={(e) => setDescrip(e.target.value)} placeholder="description" />
         </div>
 
 
         <div>
         <label>Link:</label>
-            <input type="url" name="link" placeholder= "Link" />
-        
+            <input 
+                type="url" 
+                name={link} 
+                onChange={(e) => {setLink(e.target.value)}} 
+                placeholder= "Link"
+                />
         </div>
-
-        <input type="submit" value="Add Gift" onClick = {handleSubmit} />
+            <input type="submit" value="Add Gift" onClick = {handleSubmit} />
         </form>
     )
 }
