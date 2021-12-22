3a624a18026901f7f744f1b9eef5d569301bceb0
Jennifer Lee <jenlee.tech@gmail.com>
2021-12-22 10:35:16 -0500

    header into separate components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import data from "./data.js";
 import AddGift from "./components/AddGift.js";
-import GiftList from "./components/GiftList";
+import GiftList from "./components/GiftList.js";
+import HeaderBar from "./components/HeaderBar.js";
 import React, { useState } from "react";
 
 function App() {
@@ -17,37 +18,10 @@ function App() {
     //this combines the array and the object together.
   };
 
-  const [style, setStyle] = useState("");
-
-  //Changing the style of a button here:
-  const changeButtonStyle = () => {
-    console.log("just clicked the change style function");
-    setStyle(style === "" ? "active" : "");
-  };
-
-  const [buttonText, setButtonText] = useState("Close");
-  //this is to change the text on the button upon click
-  const changeTextButton = () => {
-    setButtonText(buttonText === "Close" ? "Add Gift" : "Close");
-  };
-
   return (
     <div>
-      <div>
-        <h1> My Wishlist </h1>
-        <button
-          className={`styleButton ${style}`}
-          onClick={() => {
-            changeButtonStyle();
-            changeTextButton();
-          }}
-        >
-          {buttonText}
-        </button>
-      </div>
-
+      <HeaderBar />
       <AddGift onAdd={addGiftToList} />
-
       <GiftList gifts={gifts} />
     </div>
   );
---
+++ src/components/HeaderBar.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function HeaderBar() {
+  const [style, setStyle] = useState("");
+  //Changing the style of a button here:
+  const changeButtonStyle = () => {
+    console.log("just clicked the change style function");
+    setStyle(style === "" ? "active" : "");
+  };
+
+  const [buttonText, setButtonText] = useState("Close");
+  //this is to change the text on the button upon click
+  const changeTextButton = () => {
+    setButtonText(buttonText === "Close" ? "Add Gift" : "Close");
+  };
+  return (
+    <div>
+      <h1> My Wishlist </h1>
+      <button
+        className={`styleButton ${style}`}
+        onClick={() => {
+          changeButtonStyle();
+          changeTextButton();
+        }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+}
+
+export default HeaderBar;
