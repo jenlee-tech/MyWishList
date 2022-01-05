a3f0fb2fa40827bca7dd56851a34476873ff9a89
Jennifer Lee <jenlee.tech@gmail.com>
2022-01-04 21:21:06 -0500

    wrote function for the delete button

--- src/App.js
+++ src/App.js
@@ -18,6 +18,14 @@ function App() {
     //this combines the array and the object together.
   };
 
+  const deleteGift = (id) => {
+    //if the id equals to any id in the gifts arrray, then we delete it and then update the state of the gifts array.  Why use the filter method?  Because it will only filter the gifts we want to see with the Logical Not operator.
+
+    console.log("deleting is working", id);
+
+    setGifts(gifts.filter((gift) => gift.id !== id));
+  };
+
   const [visibleGiftList, onList] = useState(true);
   const showList = () => {
     onList(!visibleGiftList); //this is the opposite of visibleGiftList, this function will be passed in props
@@ -28,7 +36,7 @@ function App() {
       <HeaderBar showList={showList} />
       {/**conditional rendering of this AddGift component based on the change of state caused by HeaderBar component */}
       {visibleGiftList ? <AddGift onAdd={addGiftToList} /> : null}
-      <GiftList gifts={gifts} />
+      <GiftList gifts={gifts} onDelete={deleteGift} />
     </div>
   );
 }
--- src/components/GiftList.js
+++ src/components/GiftList.js
@@ -6,7 +6,13 @@ function GiftList(props) {
       {props.gifts.map((item) => (
         <li key={item.id}>
           {item.name}
-          <button type="button" className="deleteButton">
+          <button
+            type="button"
+            className="deleteButton"
+            onClick={() => {
+              props.onDelete(item.id);
+            }}
+          >
             Delete
           </button>
         </li>
